f83120883cf2058ea44b736252159e4b37d05f10
yoyold <lukas.dony@web.de>
2024-04-15 20:00:21 +0200

    feat: Conversion from UTF to number depending on byte length

--- UTFtoCodePointConverteruntitled1/src/utfconverter/UTFtoCodePointConverter.java
+++ UTFtoCodePointConverteruntitled1/src/utfconverter/UTFtoCodePointConverter.java
@@ -1,9 +1,19 @@
 package utfconverter;
 
 import java.io.UnsupportedEncodingException;
-
-public class UTFtoCodePointConverter  {
-    public static int convertToCodePoint(String utfCharacter) throws UnsupportedEncodingException{
-        return 0;
+public class UTFtoCodePointConverter {public static int convertToCodePoint(String utfCharacter) throws UnsupportedEncodingException {
+    byte[] utf8Bytes = utfCharacter.getBytes("UTF-8");
+    if (utf8Bytes.length == 1) {
+        return utf8Bytes[0];
+    } else if (utf8Bytes.length == 2) {
+        return ((utf8Bytes[0] & 0x1F) << 6) + (utf8Bytes[1] & 0x3F);
+    } else if (utf8Bytes.length == 3) {
+        return ((utf8Bytes[0] & 0xF) << 12) + ((utf8Bytes[1] & 0x3F) << 6) + (utf8Bytes[2] & 0x3F);
+    } else if (utf8Bytes.length == 4) {
+        return ((utf8Bytes[0] & 0x7) << 18) + ((utf8Bytes[1] & 0x3F) << 12)
+                + ((utf8Bytes[2] & 0x3F) << 6) + (utf8Bytes[3] & 0x3F);
+    } else {
+        throw new UnsupportedEncodingException("Invalid UTF-8 Character");
     }
+}
 }
